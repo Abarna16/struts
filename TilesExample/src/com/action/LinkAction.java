e9a155c5f62c968c5aa9148ec0c866782404947f
Abarna16 <47447958+Abarna16@users.noreply.github.com>
2019-07-03 10:32:16 +0530

    Add files via upload

---
+++ TilesExample/src/com/action/LinkAction.java
@@ -0,0 +1,42 @@
+package com.action;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.actions.DispatchAction;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
+import org.apache.struts.action.ActionForward;
+
+/**
+ *
+ * @author info@tutorials4u.net
+ */
+public class LinkAction extends DispatchAction {
+    
+   
+    /**
+     * This is the Struts action method called on
+     * http://.../actionPath?method=myAction1,
+     * where "method" is the value specified in <action> element : 
+     * ( <action parameter="method" .../> )
+     */
+    public ActionForward struts(ActionMapping mapping, ActionForm  form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        
+        return mapping.findForward("struts");
+    }
+
+    /**
+     * This is the Struts action method called on
+     * http://.../actionPath?method=myAction2,
+     * where "method" is the value specified in <action> element : 
+     * ( <action parameter="method" .../> )
+     */
+    public ActionForward hybernate(ActionMapping mapping, ActionForm  form,
+            HttpServletRequest request, HttpServletResponse response)
+            throws Exception {
+        
+        return mapping.findForward("hybernate");
+    }
+}
